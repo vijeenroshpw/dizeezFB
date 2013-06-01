10e22c5a794223413f63f20fd297736accc30f74
vijeenroshpw <hsorhteeniv@gmail.com>
2013-06-02 01:26:58 +0530

    Script for fetching data from db

---
+++ fetcher.py
@@ -0,0 +1,31 @@
+import MySQLdb
+import json
+import random
+def getQuestions(category = 0):
+	db = MySQLdb.connect('localhost','root','root','dizeeztest')
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM Questions ORDER BY RAND() LIMIT 0,50")
+	results = cursor.fetchall()
+	quests = []
+	for result in results:
+		rand_choices = [ int(x) for x in result[7].split('|') ]
+		rand_choices.append(result[8])
+		random.shuffle(rand_choices)
+		
+		quests.append(dict(question_id = result[0],clue_id=result[1],clue_name=result[2],succes_rate=result[3],clue=result[4],answer=result[5],doid=result[6],choice_list = rand_choices,correct_index=rand_choices.index(result[8])))
+	
+	db.close()
+	return  json.dumps(quests)
+
+def getDiseases():
+	db = MySQLdb.connect('localhost','root','root','dizeeztest')
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM Diseases ")
+	results = cursor.fetchall()
+	quests = []
+	for result in results:
+		quests.append(dict(disease_id = result[0],disease_name = result[1]))
+	db.close()			
+	return json.dumps(quests)
+
+
