2c1cf5c4dd447623a2293744ef0753e7f46ad3f1
vijeenroshpw <hsorhteeniv@gmail.com>
2013-06-19 22:00:03 +0530

    Included DAL using SQLAlchemy extesnion of Flask

--- app.py
+++ app.py
@@ -1,16 +1,77 @@
 from flask import Flask,render_template,request
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
 import json
-import MySQLdb
 import facebook
 import config
-import fetcher
+import random
 
-USERNAME = config.USERNAME
-PASSWORD = config.PASSWORD
-AUTH_URL = config.AUTH_URL
 APP_SECRET = config.APP_SECRET
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.CONNECTION_URI
+
+db  = SQLAlchemy(app)
+
+
+class Questions(db.Model):
+	id = db.Column(db.Integer,primary_key = True)
+	clue_id = db.Column(db.String(20))
+	clue_name = db.Column(db.String(20))
+	succes_rate = db.Column(db.Integer)
+	clue = db.Column(db.String(50))
+	answer = db.Column(db.String(50))
+	doid = db.Column(db.String(15))
+	wrong_diseases = db.Column(db.String(50))
+	correct_id = db.Column(db.Integer)
+	categories = db.Column(db.String(50))
+
+	def __init__(self, clue_id,clue_name,success_rate,clue,answer,doid,wrong_diseases,correct_id,categories):
+		self.clue_id = clue_id
+		self.clue_name = clue_name
+		self.success_rate = success_rate
+		self.clue = clue
+		self.answer = answer
+		self.doid = doid
+		self.wrong_diseases = wrong_diseases
+		self.correct_id = correct_id
+		self.categories = categories
+	
+	def __repr__(self):
+		return "<Question Clue : %s>"%(self.clue)
+
+class Diseases(db.Model):
+	id = db.Column(db.Integer,primary_key = True)
+	disease_name  = db.Column(db.String(100))
+	def __init__(self,disease_name):
+		self.disease_name = disease_name
+	def __repr__(self):
+		return "<Disease : %s>"%(self.disease_name)
+
+
+def fetch_questions(category = 0):
+	if category == 0:
+		results = Questions.query.order_by(func.rand()).limit(50).all()
+	else:
+		results = Questions.query.filter(Questions.categories.like("%" + str(category) + "%")).order_by(func.rand()).limit(50).all()
+	
+	quests = []
+	for result in results:
+		rand_choices = [ int(x) for x in result.wrong_diseases.split("|") ]
+		rand_choices.append(result.correct_id)
+		random.shuffle(rand_choices)
+		quests.append(dict(question_id = result.id,clue_id = result.clue_id,clue_name = result.clue_name,success_rate = result.succes_rate,clue = result.clue,answer = result.answer,doid = result.doid,choice_list = rand_choices,correct_index = rand_choices.index(result.correct_id)))
+	return json.dumps(quests)
+
+	
+def fetch_diseases():
+	results  = Diseases.query.all()
+	diseases = []
+	for result in results:
+		diseases.append(dict(disease_id = result.id,disease_name = result.disease_name))
+	return json.dumps(diseases)
+
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -30,17 +91,20 @@ def game():
 	else:
 		return render_template('game.html')
 
+
 @app.route('/test',methods=['GET','POST'])
 def test():
 	return 	render_template('test.html')
 
+
 @app.route('/questions',methods=['GET','POST'])
 def questions():
-	return fetcher.getQuestions()
+	return fetch_questions()
+
 
 @app.route('/diseases',methods=['GET','POST'])
 def diseases():	
-	return fetcher.getDiseases()
+	return fetch_diseases()
 
 if __name__ == '__main__':
 	app.run()
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 USERNAME='root'
 PASSWORD='root'
-DBNAME = 'dizeeztest1'
+DBNAME = 'dizeezalchemy'
 DBHOST = 'localhost'
+
+###### Connection URI ######
+CONNECTION_URI = "mysql://" + USERNAME + ":" + PASSWORD + "@" + DBHOST + "/" + DBNAME
+
+
 AUTH_URL = "https://www.facebook.com/dialog/oauth?client_id=159866620823022&redirect_uri=http://apps.facebook.com/infolinksgec/game&scope=friends_games_activity,user_games_activity"
+
 APP_SECRET = "62c799259d7c6754606ae0db72c9db3c"
