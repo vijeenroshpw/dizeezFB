0826f3ad6cdf609faf0e6755753e8ec5f5368505
Max Nanis <max@maxnanis.com>
2013-07-11 16:01:07 -0700

    major clenaup and organization of code.

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask,render_template,request
+from flask import Flask, request, jsonify, url_for, redirect, render_template
+from flask.views import View
+
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
-from flask.ext.restful import Resource, Api
+from flask.ext.restful import reqparse, abort, Api, Resource
 
 import json, facebook, config, random, sys, urllib
 
-#-- fetch necessary app parameters to global scope 
+#
+# A P P  C O N F I G
+#
+#-- Fetch necessary app parameters to global scope
 APP_SECRET = config.APP_SECRET
 AUTH_URL = config.AUTH_URL
 APP_ID = config.APP_ID
 
-#-- global app object
+#-- Global app object
 app = Flask(__name__)
-
-#-  connection url , specifying location, port , username , password , dbname 
-app.config['SQLALCHEMY_DATABASE_URI'] = config.CONNECTION_URI
-
-#-- Global database handle
-db  = SQLAlchemy(app)
 
 #-- Global restful api handle
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.CONNECTION_URI
+db = SQLAlchemy(app)
+
+#
+# U T I L S
+#
+
 
 #
 # M O D E L S
@@ -31,35 +37,43 @@ class Question(db.Model):
     that can have a doid, it have many categories, and it can have many choices. 1 of the choices
     should be flagged as correct
   '''
-  id              = db.Column(db.Integer, primary_key = True)
-  text            = db.Column(db.String())
+  id          = db.Column(db.Integer, primary_key = True)
+  text        = db.Column(db.String(240))
   created     = db.Column(db.DateTime)
 
-  categories       = db.relationship('Category', backref=db.backref('question',  lazy='select'))
-  choices       = db.relationship('Choice', backref=db.backref('question',  lazy='select'))
+  categories  = db.relationship('Category', backref=db.backref('question',  lazy='select'))
+  choices     = db.relationship('Choice', backref=db.backref('question',  lazy='select'))
 
   def __repr__(self):
     return "<Question : %s>"%(self.text)
+
+  def json_view(self):
+    return {  'id'          : self.id,
+              'text'        : self.text }
 
 class Choice(db.Model):
   '''
     A choice is a potential answer to a question. A question can have unlimited choices. A question
     must have at least 1 choice that is flagged as correct
   '''
-  id            = db.Column(db.Integer,primary_key = True)
-  text          = db.Column(db.String())
-  created     = db.Column(db.DateTime)
+  id            = db.Column(db.Integer, primary_key = True)
+  text          = db.Column(db.String(240))
+  created       = db.Column(db.DateTime)
+
+  question_id   = db.Column(db.Integer, db.ForeignKey('question.id'))
 
   def __init__(self, disease_name):
     self.disease_name = disease_name
 
   def __repr__(self):
-    return "<Disease : %s>"%(self.text)
+    return "<Choice : %s>"%(self.text)
 
 class Category(db.Model):
-  id    = db.Column(db.Integer, primary_key = True)
-  text  = db.Column(db.String())
-  created     = db.Column(db.DateTime)
+  id            = db.Column(db.Integer, primary_key = True)
+  text          = db.Column(db.String(240))
+  created       = db.Column(db.DateTime)
+
+  question_id   = db.Column(db.Integer, db.ForeignKey('question.id'))
 
   def __repr__(self):
     return "<Category : %s>"%(self.text)
@@ -67,94 +81,27 @@ class Category(db.Model):
 #
 # A P I
 #
+class Questions(Resource):
+  '''
+    GET: Returns list of questions
 
-
-
-#-- Questions Resource ,    /api/v1/questions/1 returns list of questions (in JSON) questin belong to category 1
-class QuestionsResource(Resource):
-  def get(self,category):
-    return fetch_questions(int(category))
-    
-#-- Diseases Resource     /api/v1/diseases  returns list of all diseases (in JSON)
-class DiseasesResource(Resource):
+  '''
   def get(self):
-    return fetch_diseases()
+    questions = Question.query.all()
+    return jsonify(objects=[i.json_view() for i in questions])
 
-#-- /api/v1/questchoice/<category>/<num_choices> returns questions of category , with num_choice choices
-class QuestChoiceResource(Resource):
-  def get(self,category,num_choices):
-    return fetch_questchoice(int(category),int(num_choices))
-    
+#-- Config API urls
+api.add_resource(Questions, '/api/v1/questions')
 
-#-- Configuring api urls
-api.add_resource(QuestionsResource,'/api/v1/questions/<string:category>')
-api.add_resource(DiseasesResource,'/api/v1/diseases')
-api.add_resource(QuestChoiceResource,'/api/v1/questchoice/<string:category>/<string:num_choices>')
-
-
-#-- Fetches questions of given category
-def fetch_questions(category = 0):
-  if category == 0:
-    results = Questions.query.order_by(func.rand()).limit(50).all()
-  else:
-    results = Questions.query.filter(Questions.categories.like("%" + str(category) + "%")).order_by(func.rand()).limit(50).all()
-  
-  quests = []
-  for result in results:
-    rand_choices = [ int(x) for x in result.wrong_diseases.split("|") ]
-    rand_choices.append(result.correct_id)
-    random.shuffle(rand_choices)
-    quests.append(dict(question_id = result.id,clue_id = result.clue_id,clue_name = result.clue_name,success_rate = result.succes_rate,clue = result.clue,answer = result.answer,doid = result.doid,choice_list = rand_choices,correct_index = rand_choices.index(result.correct_id)))
-  return quests
-
-#-- Fetches all diseases
-def fetch_diseases():
-  results  = Diseases.query.all()
-  diseases = []
-  for result in results:
-    diseases.append(dict(disease_id = result.id,disease_name = result.disease_name))
-  return diseases
-
-
-
-#-- Returns a random list of  n diseases,including the correct_disease index
-def fetch_choices(correct_choice,num_choice):
-  rand_list = []
-  while len(rand_list) != (num_choice-1) :
-    rand_num = random.randint(1,836)
-    if rand_num not in rand_list:
-      if rand_num != correct_choice:
-        rand_list.append(rand_num)
-  rand_list.append(correct_choice)
-  random.shuffle(rand_list)
-  #-- the location of correct ID
-  correct_id = rand_list.index(correct_choice)
-  rand_choices = []
-  for i in rand_list:
-    disease = Diseases.query.get(i)
-    rand_choices.append(dict(disease_id=disease.id,disease_name=disease.disease_name))
-  return (rand_choices,correct_id)
-  
-
-
-##-- Fetches questions of given category, along with "num_choices", returns a list of diseases , id of correct disease
-def fetch_questchoice(category=0,num_choices = 3):
-  if category == 0:
-    results = Questions.query.order_by(func.rand()).limit(50).all()
-  else:
-    results = Questions.query.filter(Questions.categories.like("%" + str(category) + "%")).order_by(func.rand()).limit(50).all()
-
-  quests = []
-  for result in results:
-    choice_list,correct_id = fetch_choices(result.correct_id + 1,num_choices)
-    quests.append(dict(question_id = result.id,clue_id = result.clue_id,clue_name = result.clue_name,success_rate = result.succes_rate,clue = result.clue,answer = result.answer,doid = result.doid,choices = choice_list,correct_index = correct_id))
-  return quests
-
-@app.route('/',methods=['GET','POST'])
+#
+# Boring old standard routes / need to figure 
+# out what importance these actually have?
+#
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html',auth_url = AUTH_URL)
+  return render_template('index.html', auth_url = AUTH_URL)
 
-@app.route('/game',methods=['GET','POST'])
+@app.route('/game', methods=['GET', 'POST'])
 def game():
   if request.method == 'POST':
     if 'signed_request' in request.form:
@@ -175,15 +122,11 @@ def game():
         print player['user']['name'],player['score'],player['user']['id']
 
       try:
-        #stuff = graph.get_object("https://graph.facebook.com/me/picture",redirect=False)
         stuff = urllib.urlopen("https://graph.facebook.com/me/picture?redirect=false&access_token="+sr_data['oauth_token'])
-          
         print stuff.read()
       except:
         print sys.exc_info()
 
-      #Homework to be done 
-                           #1 getting profile pics of friends
     else:
       print " No Signed Request Obtained"
 
@@ -191,10 +134,13 @@ def game():
   else:
     return render_template('game.html')
 
+#
+# Main App Center
+#
 
-@app.route('/play/<category>',methods=['GET','POST'])
-def mtest(category=0):
-  return render_template('app.html',fetch_url = '/api/v1/questions/'+category)
+# Create DB
+db.create_all()
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.debug = True
+    app.run()
--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 USERNAME='root'
-PASSWORD='root'
-DBNAME = 'dizeezalchemy1'
+PASSWORD=''
+DBNAME = 'dizeez'
 DBHOST = 'localhost'
 
 ###### Connection URI ######
 CONNECTION_URI = "mysql://" + USERNAME + ":" + PASSWORD + "@" + DBHOST + "/" + DBNAME
 
-
-AUTH_URL = "https://www.facebook.com/dialog/oauth?client_id=159866620823022&redirect_uri=http://apps.facebook.com/infolinksgec/game&scope=friends_games_activity,user_games_activity,publish_actions"     # auth_url of infolinksgec 
-APP_SECRET = "62c799259d7c6754606ae0db72c9db3c"     #app_secret of infolinks gec
+# Auth_url of infolinksgec
+AUTH_URL = "https://www.facebook.com/dialog/oauth?client_id=159866620823022&redirect_uri=http://apps.facebook.com/infolinksgec/game&scope=friends_games_activity,user_games_activity,publish_actions"
+# App_secret of infolinks gec
+APP_SECRET = "62c799259d7c6754606ae0db72c9db3c"     
 
 APP_ID = "159866620823022"       # app_id of infolinksgec
